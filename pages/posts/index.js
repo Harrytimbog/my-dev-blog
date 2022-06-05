c7336c9b93f3b7599c466ab945f21a79b894b5e7
Timilehin Arigbede <Harriemannie@gmail.com>
2022-06-05 20:17:43 +0100

    Adding the All Post Page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React framework for production - It ships with SSR. I love it so much',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-finished-with-nextjs',
+    title: 'Getting Finished with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React framework for production - It ships with SSR. I love it so much',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-busy-with-nextjs',
+    title: 'Getting Busy with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React framework for production - It ships with SSR. I love it so much',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-naughty-with-nextjs',
+    title: 'Getting Naughty with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React framework for production - It ships with SSR. I love it so much',
+    date: '2022-02-10',
+  },
+];
+
 
 const AllPostsPage = () => {
-  return (
-    <div>AllPostsPage</div>
-  )
+  return <AllPosts posts={DUMMY_POSTS} />
 }
 
 export default AllPostsPage;
