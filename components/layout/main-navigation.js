659a5c2afbe41aeb328391113f198319383db781
Timilehin Arigbede <Harriemannie@gmail.com>
2022-06-05 17:26:25 +0100

    Add Logo and Add styles to Hero section

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Link from 'next/link';
+import classes from './main-navigation.module.css';
+import Logo from './logo';
 
 const MainNavigation = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <Link href='/'>
         <a>
           <Logo />
@@ -11,8 +13,8 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li><Link href="/posts"><a>Posts</a></Link></li>
-          <li><Link href="/contact"><a>Contact</a></Link></li>
+          <li><Link href="/posts">Posts</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
