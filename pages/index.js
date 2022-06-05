3aff37b87c23e9965595688723d21f41f65ab64f
Timilehin Arigbede <Harriemannie@gmail.com>
2022-06-05 19:58:03 +0100

    Rendering Dummy Post Data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,43 @@
 import { Fragment } from "react";
+import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from '../components/home-page/hero';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React framework for production - It ships with SSR. I love it so much',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-finished-with-nextjs',
+    title: 'Getting Finished with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React framework for production - It ships with SSR. I love it so much',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-busy-with-nextjs',
+    title: 'Getting Busy with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React framework for production - It ships with SSR. I love it so much',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-naughty-with-nextjs',
+    title: 'Getting Naughty with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React framework for production - It ships with SSR. I love it so much',
+    date: '2022-02-10',
+  },
+];
 
 const HomePage = () => {
   return (
     <Fragment>
       <Hero />
-      {/* <FeaturedPosts /> */}
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   )
 }
