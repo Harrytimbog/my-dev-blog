8373710fb8d780e7ae19260e642016177400cd55
Timilehin Arigbede <Harriemannie@gmail.com>
2022-06-06 12:29:24 +0100

    Using Markdown data for Rendering Posts

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,25 @@
 import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from '../components/home-page/hero';
+import { getAllFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is the React framework for production - It ships with SSR. I love it so much',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-finished-with-nextjs',
-    title: 'Getting Finished with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is the React framework for production - It ships with SSR. I love it so much',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-busy-with-nextjs',
-    title: 'Getting Busy with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is the React framework for production - It ships with SSR. I love it so much',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-naughty-with-nextjs',
-    title: 'Getting Naughty with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NextJS is the React framework for production - It ships with SSR. I love it so much',
-    date: '2022-02-10',
-  },
-];
-
-const HomePage = () => {
+const HomePage = (props) => {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   )
+}
+
+export function getStaticProps() {
+  const featuredPosts = getAllFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts
+    }
+  };
 }
 
 export default HomePage;
